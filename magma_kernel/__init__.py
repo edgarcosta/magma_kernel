d2127b01e1a699f18acea2b5e0c029ea2352cc9f
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-01-24 16:56:56 -0500

    version things

--- magma_kernel/__init__.py
+++ magma_kernel/__init__.py
@@ -1,5 +1,8 @@
 """A magma kernel for Jupyter"""
 
+from codecs import open
+from os import path
+
 def readfile(filename):
     with open(filename,  encoding='utf-8') as f:
         return f.read()
