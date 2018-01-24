9a03dcf20737dbe5b638329cd5784aed351679e2
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-01-24 16:54:56 -0500

    version things

--- magma_kernel/__init__.py
+++ magma_kernel/__init__.py
@@ -1,4 +1,7 @@
 """A magma kernel for Jupyter"""
 
-from kernel import __version__
+def readfile(filename):
+    with open(filename,  encoding='utf-8') as f:
+        return f.read()
+__version__ = readfile(path.join(path.dirname(__file__), 'VERSION'));
 assert __version__ #silence pyflakes
